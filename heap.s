dcc4e7b9deb3f72468a43818ccd4ac3bde6f8766
ojsj18 <juhnior88@gmail.com>
2022-04-11 19:49:48 -0300

    doing

--- heap.s
+++ heap.s
@@ -1,37 +1,45 @@
 .section .data
-   tam_heap: .quad 0
-   inicio:   .quad 0
-   header:   .quad 16
+   tam_heap:     .quad 0
+   inicio:       .quad 0
+   ini_heap:     .quad 0
+   header:       .quad 16
+   bloco:        .quad 64
    str1:     .string "começou a baixaria\n"
    str2:     .string "valor da heap %p\n"
    str3:     .string "status %d |tamanho %d\n"
+   str4:     .string "inicio %d |tamanho %d | ini_heap %d \n"
 .section .text
 .globl main
 
 #a ideia é a mesma que o em c, porem aqui eu nao preciso calcular o inicio e o fim
 #esta tudo nas variavel globais (lembrar de atualizalas)
+#lembrar que andamos do começo do bloco livre
+#ou seja as flags estao para tras
+
 iniciaAlocador:
    pushq %rbp
    movq %rsp,%rbp
-   movq $12, %rax               #chama a brk
+
+   movq $12, %rax              #chama a brk
    movq $0, %rdi               #chama a brk com 0(retorna topo em rax)
    syscall
 
-#####com problema
-  #abre espaço pro primeiro header
-   addq $100,%rax       
-   movq %rax, tam_heap
-   movq %rax, inicio
+  #os headers ficaram em -8 e -16 entao tem que andar 16 depois do inicio
+   movq %rax,ini_heap    #ini-heap := endereço da heap
+   addq header, %rax     # rax:= header
+   movq %rax, inicio     # inicio := ini_heap+header 
+   addq bloco,%rax       # rax:= ini_heap+header+novo_bloco
+   movq %rax, tam_heap  
+
    movq %rax,%rdi
    movq $12, %rax               #chama a brk
    syscall
 
    #coloca o header
    movq inicio,%rax
-   movq $0,-8(%rax)  #STATUS
-   movq $10,-16(%rax) #TAMANHO
+   movq $0,-16(%rax)  #STATUS
+   movq bloco,-8(%rax) #TAMANHO
 
-   ##############
    pop %rbp
    ret
 
@@ -43,12 +51,45 @@ finalizaAlocador:
    movq $12,%rax
 
    #preciso calcular o header no inicio
-   movq inicio,%rdi
+   movq ini_heap,%rdi
    syscall
 
    pop %rbp
    ret
-   
+
+imprime_infs:
+   pushq %rbp
+   movq %rsp,%rbp
+
+   movq $str4,%rdi 
+   movq inicio,%rsi
+   movq tam_heap,%rdx
+   movq ini_heap,%rcx
+   call printf
+
+   pop %rbp
+   ret
+
+imprime_mapa:
+   pushq %rbp
+   movq %rsp,%rbp
+
+   movq inicio,%rax      #i = inicio
+   while_imprime:
+   cmpq %rax,tam_heap #if i<tam_heap
+   jl fim_while_imprime
+   movq $str3,%rdi
+   movq -16(%rax),%rsi
+   movq -8(%rax),%rdx
+   call printf
+
+   addq %rdx,%rax #i=i+tam_bloco
+   jmp  while_imprime
+
+   fim_while_imprime:
+   pop %rbp
+   ret
+
 politica_de_escolha:
    pushq %rbp
    movq %rsp,%rbp
@@ -58,9 +99,9 @@ politica_de_escolha:
    while_percorre:
    cmpq %rdx,tam_heap #if i<tam_heap
    jl fim_while_percorre
-   cmpq $1,-8(%rdx)  # ve se ta livre
+   cmpq $1,8(%rdx)  # ve se ta livre
    jne fim_if_livre
-   cmpq -16(%rdx),%rax   #se tamanho no bloco e disponivel
+   cmpq 16(%rdx),%rax   #se tamanho no bloco e disponivel
    jle fim_if_livre
    movq %rdx,%rdi
    pop %rbp
@@ -81,8 +122,8 @@ politica_de_escolha:
    syscall
 
    #coloca o header e devolve o novo bloco
-   movq $0,-16(%rdx)  #TAMANHO
-   movq $0,-8(%rdx)   #STATUS
+   movq $0,16(%rdx)  #TAMANHO
+   movq $0,8(%rdx)   #STATUS
    movq %rdx,%rdi
    pop %rbp
    ret
@@ -96,52 +137,15 @@ alocaMem:
    addq $16, %rsp
 
 main:
-   pushq %rbp       #bateu a duvida do pq preciso salvar rbp nesse caso                               
+   pushq %rbp    
    movq %rsp, %rbp 
 
    movq $str1, %rdi                   
    call printf
-
-   movq $12, %rax              #chama a brk
-   movq $0, %rdi              #chama a brk com 10 de
-   syscall       
-   movq %rax, %rsi           
-   movq $str2, %rdi                   
-   call printf
    
    call iniciaAlocador
-
-   movq $12, %rax             #chama a brk
-   movq $0, %rdi              #tamanho heap
-   syscall       
-   movq %rax, %rsi           
-   movq $str2, %rdi                   
-   call printf
-
-   movq inicio,%rax
-   movq -8(%rax),%rsi  #to indo pra tras ou pra frente?
-   movq -16(%rax),%rdx
-   movq $str3, %rdi
-   call printf
-
-
-#teste da funcao  politica_de_escolha
-   push $16
-   call politica_de_escolha
-   addq $8,%rsp #libera parametros
-
-   movq %rdi, %rsi           
-   movq $str2, %rdi                   
-   call printf
-
-#teste da funcao  politica_de_escolha de novo
-   push $32
-   call politica_de_escolha
-   addq $8,%rsp #libera parametros
-
-   movq %rdi, %rsi           
-   movq $str2, %rdi                   
-   call printf
+   call imprime_infs
+   call imprime_mapa
 
    movq $60, %rax
    syscall
